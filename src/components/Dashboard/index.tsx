5d9bd79cd8c4a96a88c32089bbf779b6773e5814
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-05-24 16:56:13 -0300

    estilizando app

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import {Container} from './styles'
+import { Summary } from '../Summary/index'
+import { TransactionsTable } from '../TransactionsTable'
+
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Summary/>
+            <TransactionsTable/>
+        </Container>
+    )
+}
--- src/global.ts
+++ src/global.ts
@@ -6,14 +6,14 @@ export const GlobalStyle = createGlobalStyle `
         --background: #f0f2f5;
         --red: #E52E4D;
         --blue: #5429CC;
-
+        --green: #33CC95;
         --blue-light: #6933FF;
 
         --text-title: #363F5F;
         --text-body: #969CB3;
 
         --background: #F0F2F5;
-        --shape: #FFFFF;
+        --shape: #FFFFFF;
     }
 
     * {
