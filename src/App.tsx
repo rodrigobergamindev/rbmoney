e99d3ed20133bdb7da8f102fbf4dc0ac120ad539
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-05-31 16:54:13 -0300

    implementando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header/index';
 import {GlobalStyle} from './global'
+import Modal from 'react-modal';
+import {useState} from 'react'
+import { NewTransactionModal } from './components/NewTransactionModal/NewTransactionModal';
 
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
   return (
     <>
-     <Header/>
+     <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
      <Dashboard/>
+     <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
     <GlobalStyle/>
     </>
   );
